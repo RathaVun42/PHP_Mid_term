028b7a820c5072fe7c2b697b1b63b42dd3b3ebbc
ratha <rathavun449@gmail.com>
2026-01-25 20:01:09 +0700

    register check

--- pages/register.php
+++ pages/register.php
@@ -3,31 +3,55 @@
     // include '../../includes/navbar.inc.php';
     //include $_SERVER['DOCUMENT_ROOT'] . '/G19BCSY3A/includes/header.inc.php';
     //include $_SERVER['DOCUMENT_ROOT'] . '/G19BCSY3A/includes/navbar.inc.php';
-    if(isset($_POST['email'])){
-        echo $_POST['email'];
+    // if(isset($_POST['email'])){
+    //     echo $_POST['email'];
+    // }
+    $nameErr = $usernameErr = $passErr = "";
+    $name = $username = "";
+    if(isset($_POST['name'], $_POST['username'],$_POST['pass'], $_POST['confirmPass'])){
+        $name = $_POST['name'];
+        $username = $_POST['username'];
+        $pass = $_POST['pass'];
+        $confirmPass = $_POST['confirmPass'];
+        if(empty($name)){
+            $nameErr = "Please input name";
+        }
+        if(empty($username)){
+            $usernameErr = "Please input username";
+        }
+        if(empty($pass)){
+            $passErr = "Please input password";
+        }
     }
 ?>
+    
 
     
     <form class="col-lg-5 col-sm-5 mx-auto" method="post" action="./?page=register ">
         <h1>Register page</h1>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label  class="form-label">Name</label>
+            <input name="name" type="text" class="form-control <?= empty($nameErr) ? '':'is-invalid'; ?>" value="<?= $name ?>" id="exampleInputEmail1">
+            <div class="invalid-feedback"><?= $nameErr ?></div>
+        </div>
+        <div class="mb-3">
+            <label  class="form-label">Username</label>
+            <input name="username" type="text" class="form-control <?= empty($usernameErr) ? '':'is-invalid'; ?>"  value="<?= $username ?>" id="exampleInputEmail1">
+            <div class="invalid-feedback"><?= $usernameErr ?></div>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input name="pass" type="password" class="form-control" id="exampleInputPassword1">
+            <input name="pass" type="password" class="form-control <?= empty($passErr) ? '':'is-invalid'; ?>" value="<?= $pass ?>" id="exampleInputPassword1">
+            <div class="invalid-feedback"><?= $passErr ?></div>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Confirm password</label>
-            <input name="confirmPass" type="password" class="form-control" id="exampleInputPassword1">
+            <input name="confirmPass" type="password" class="form-control <?= ($pass===$confirmPass)?'':'is-invalid' ?> ?>" id="exampleInputPassword1">
         </div>
-        <div class="mb-3 form-check">
+        <!-- <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
             <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
+        </div> -->
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
